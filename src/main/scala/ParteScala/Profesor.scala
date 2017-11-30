3a804e36cf7ed1b58b40f91cb44275ec2c3d1ebb
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2017-11-30 09:38:40 -0600

    Update teachers' classes.

--- src/main/scala/ParteScala/Profesor.scala
+++ src/main/scala/ParteScala/Profesor.scala
@@ -6,12 +6,12 @@ class Profesor(val nombre: String) extends Ordered[Profesor] with Profesional {
   val this.nombre: String = nombre
   this.ocupacion = "Profesor"
 
-  val cursos: ArbolDosTres[Curso] = new ArbolDosTres[Curso]()
+  val cursosBachillerato: ArbolDosTres[CursoBachillerato] = new ArbolDosTres[CursoBachillerato]()
+
+  def CursosBachillerato: ArbolDosTres[CursoBachillerato] = this.cursosBachillerato
 
   // Ordenamiento por nombre.
   override def compare(that: Profesor): Int = this.nombre.compareTo(that.nombre)
 
   override def toString: String = s"Nombre: $nombre\nOcupación: $ocupacion\n"
-
-  def getCursos: ArbolDosTres[Curso] = cursos
 }
--- src/main/scala/ParteScala/ProfesorDoctorado.scala
+++ src/main/scala/ParteScala/ProfesorDoctorado.scala
@@ -1,9 +1,12 @@
 package ParteScala
-
 import PackArbolDosTres._
 
 class ProfesorDoctorado(nombre: String) extends Profesor(nombre) {
   this.ocupacion += " (Ph.D)"
+
+  val cursosPosgrado: ArbolDosTres[CursoPosgrado] = new ArbolDosTres[CursoPosgrado]()
+
+  def CursosPosgrado: ArbolDosTres[CursoPosgrado] = cursosPosgrado
 
   override def compare(that: Profesor): Int = {
     if (that.isInstanceOf[ProfesorDoctorado]) {
